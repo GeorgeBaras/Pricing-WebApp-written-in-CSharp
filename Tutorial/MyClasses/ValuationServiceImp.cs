e5fd6bc0daac37fe8ebd6590a4fe747b41ec74ff
GeorgeBaras <georgebaras@outlook.com>
2017-01-22 18:19:16 +0200

    added files for log4net- pending changes in location

--- Tutorial/MyClasses/ValuationServiceImp.cs
+++ Tutorial/MyClasses/ValuationServiceImp.cs
@@ -9,9 +9,11 @@ namespace Tutorial.MyClasses
     {
         private ValuationDAO valuationDAO;
         private ValuationCalculator valuationCalculator;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public decimal valueVehicle(Vehicle vehicle)
         {
+            log.Info("Got into the vehicle valuation for vehicle-lookupCode: "+vehicle.LookupCode);
             return this.ValuationCalculator.calculate(this.ValuationDAO.getPriceRecord(vehicle.LookupCode),vehicle.Mileage);
         }
 
