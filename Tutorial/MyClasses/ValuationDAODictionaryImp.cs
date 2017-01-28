53a8dccefbd24912f8e95d2cc70e2d6e6ee5eaf7
GeorgeBaras <georgebaras@outlook.com>
2017-01-28 18:13:13 +0200

    Added ValuationDAO List&Dictionary implementations

---
+++ Tutorial/MyClasses/ValuationDAODictionaryImp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial.MyClasses 
+{
+    public class ValuationDAODictionaryImp : ValuationDAO
+    {
+        private Dictionary<String, PriceRecord> priceRecordsDictionary { get; set; }
+
+        public ValuationDAODictionaryImp(Dictionary<String, PriceRecord> priceRecordsDictionary)
+        {
+            this.priceRecordsDictionary = priceRecordsDictionary;
+        }
+
+        public PriceRecord getPriceRecord(string lookupCode)
+        {
+            if (priceRecordsDictionary.ContainsKey(lookupCode)){
+                return this.priceRecordsDictionary[lookupCode];
+            }
+            else {
+                return null;
+            }
+        }
+
+        public void addPriceRecord(PriceRecord priceRecord) {
+            this.priceRecordsDictionary.Add(priceRecord.getLookupCode(), priceRecord);
+        }
+}
+}
---
+++ Tutorial/MyClasses/ValuationDAOListImp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial.MyClasses
+{
+    public class ValuationDAOListImp : ValuationDAO
+    {
+        private List<PriceRecord> priceRecordsList { get; set; }
+
+        public ValuationDAOListImp(List<PriceRecord> priceRecordsList) {
+            this.priceRecordsList = priceRecordsList;
+        }
+
+        public PriceRecord getPriceRecord(string lookupCode)
+        {
+            foreach (PriceRecord priceRecord in priceRecordsList)
+            {
+                if (priceRecord.getLookupCode().Equals(lookupCode)) {
+                    return priceRecord;
+                }
+            }
+            return null;
+        }
+
+        public void addPriceRecord(PriceRecord priceRecord)
+        {
+            this.priceRecordsList.Add(priceRecord);
+        }
+    }
+}
