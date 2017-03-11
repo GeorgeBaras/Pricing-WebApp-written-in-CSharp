2b7d8ee0732669a94bda762481c71c33bea4fa0b
Unknown <georgebaras@outlook.com>
2017-03-11 17:48:32 +0200

    DB generator

---
+++ Tutorial.Tests/DataBaseUT/DatabaseGeneratorUT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tutorial.My_EntityClasses;
+using Tutorial.MyClasses;
+using System.Collections.Generic;
+
+namespace Tutorial.Tests.DataBaseUT
+{
+    [TestClass]
+    public class DatabaseGeneratorUT
+    {
+        [TestMethod]
+        public void TestGenerateVehicles()
+        {
+            List<Vehicle> vehicles = DatabaseGenerator.generateVehicles(1000);
+            Assert.AreEqual(1000, vehicles.Count, "Vehicles were not created successfully");
+        }
+
+
+        [TestMethod]
+        public void TestGeneratePriceBands()
+        {
+            List<PriceBand> priceBands = DatabaseGenerator.generatePriceBands(100);
+            Assert.AreEqual(100, priceBands.Count, "PriceBands were not created successfully");
+        }
+
+        [TestMethod]
+        public void TestGeneratePriceRecords()
+        {
+            List<PriceRecord> priceRecords = DatabaseGenerator.generatePriceRecords(1000,10);
+            Assert.AreEqual(1000, priceRecords.Count, "PriceRecords were not created successfully");
+        }
+    }
+}
---
+++ Tutorial.Tests/ModelWorld/ModelWorld.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tutorial.EF_DBContext;
+using Tutorial.MyClasses;
+using Microsoft.Practices.Unity;
+using Tutorial.EF_DBContext_Repositories;
+using System.Data.Entity;
+using Tutorial.My_EntityClasses;
+
+namespace Tutorial.Tests.ModelWorld
+{
+    [TestClass]
+    public class ModelWorld
+    {
+        static IUnityContainer myContainer = UnityContainerSingleton.getContainer();
+        static VehicleRepository vehicleRepository = (VehicleRepository)myContainer.Resolve<IRepository>("vehicleRepository");
+        static PriceRecordRepository priceRecordRepository = (PriceRecordRepository)myContainer.Resolve<IRepository>("priceRecordRepository");
+        static appDBContext db = (appDBContext)myContainer.Resolve<DbContext>("applicationDBContext");
+
+        [Ignore]
+        [TestMethod]
+        public void GenerateDatabase1k()
+        {
+            int numberOfRecords = 1000;
+            int priceBandsPerPriceRecord = 3;
+            vehicleRepository.addVehicleList(DatabaseGenerator.generateVehicles(numberOfRecords));
+            priceRecordRepository.addPriceRecordList(DatabaseGenerator.generatePriceRecords(numberOfRecords, priceBandsPerPriceRecord));
+
+            Assert.AreEqual(numberOfRecords, vehicleRepository.getAllEntries().Count, "Not all vehicle entries were inserted in the database successfully");
+            Assert.AreEqual(numberOfRecords, priceRecordRepository.getAllEntries().Count, "Not all priceRecord entries were inserted in the database successfully");
+        }
+
+        [Ignore] // takes about 45'
+        [TestMethod]
+        public void GenerateDatabase10k()
+        {
+            int numberOfRecords = 10000;
+            int priceBandsPerPriceRecord = 5;
+            vehicleRepository.addVehicleList(DatabaseGenerator.generateVehicles(numberOfRecords));
+            priceRecordRepository.addPriceRecordList(DatabaseGenerator.generatePriceRecords(numberOfRecords, priceBandsPerPriceRecord));
+
+            Assert.AreEqual(numberOfRecords, vehicleRepository.getAllEntries().Count, "Not all vehicle entries were inserted in the database successfully");
+            Assert.AreEqual(numberOfRecords, priceRecordRepository.getAllEntries().Count, "Not all priceRecord entries were inserted in the database successfully");
+        }
+
+        [Ignore]
+        public void GenerateDatabase100k()
+        {
+            int numberOfRecords = 100000;
+            int priceBandsPerPriceRecord = 10;
+            vehicleRepository.addVehicleList(DatabaseGenerator.generateVehicles(numberOfRecords));
+            priceRecordRepository.addPriceRecordList(DatabaseGenerator.generatePriceRecords(numberOfRecords, priceBandsPerPriceRecord));
+
+            Assert.AreEqual(numberOfRecords, vehicleRepository.getAllEntries().Count, "Not all vehicle entries were inserted in the database successfully");
+            Assert.AreEqual(numberOfRecords, priceRecordRepository.getAllEntries().Count, "Not all priceRecord entries were inserted in the database successfully");
+
+        }
+        [Ignore]
+        [TestMethod]
+        public void nukeDataBase() {
+            db.Database.Delete();
+            Assert.IsFalse(db.Database.Exists());
+        }
+    }
+}
